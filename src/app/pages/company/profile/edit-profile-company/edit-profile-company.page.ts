fccced9ddf6c24fbb96ee9a39848b7dabed9e736
Sebastian Abarca <sebastian.abarca.p@gmail.com>
2024-09-18 23:06:02 -0300

    refactorizacion edit-profile-formulario

--- src/app/pages/company/profile/edit-profile-company/edit-profile-company.page.ts
+++ src/app/pages/company/profile/edit-profile-company/edit-profile-company.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { IonInput } from '@ionic/angular';
+import { emailValidation, textValidaton } from 'src/app/utils/validation-functions';
 
 @Component({
   selector: 'app-edit-profile-company',
@@ -17,40 +17,19 @@ export class EditProfileCompanyPage implements OnInit {
     about: "somos el epítome de la eficiencia corporativa. Nuestro equipo altamente capacitado se especializa en asistir a reuniones interminables y redactar correos electrónicos extremadamente importantes. Con un enfoque inquebrantable en maximizar las pausas para el café y optimizar la procrastinación, hemos alcanzado un nivel de profesionalismo que es difícil de igualar Nuestros proyectos siempre están en camino de ser iniciados, y nuestros plazos son tan flexibles que incluso se estiran hasta el infinito. Nos enorgullecemos de nuestra capacidad para gestionar tareas cruciales justo cuando ya no son tan cruciales. La calidad es nuestra segunda prioridad... justo después de encontrar el GIF perfecto para los chats de equipo. Si buscas un socio que pueda llevar tu proyecto a la cima de la lista de cosas por hacer (eventualmente), no busques más. ¡Estamos aquí para posponerlo todo con estilo y profesionalismo!"
   }
 
+  inputModel = this.user.phone;
+
+  //Validadores
+  nameIsCorrect:boolean = true;
+  emailIsCorrect:boolean = true;
+
+  //Mensajes de error
+  emailErrorMessage = "";
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  isEmail(email: string): boolean {
-    
-    if (!email) {
-      console.log("que chucha")
-      return false; 
-    }
-    let emailParts = email.split('@');
-
-    if(emailParts.length != 2){
-      return false;
-    }
-    emailParts = emailParts[1].split('.');
-    if(emailParts.length != 2){
-      return false;
-    }
-
-    return true;
-  }
-
-  validateData(formData: NgForm){
-    if (formData.valid && this.isEmail(formData.value.correo)){
-      console.log("formName Valido")
-    }else{
-      console.log("formName Invalido")
-    }
-  }
-
-
-  inputModel = this.user.phone;
 
   @ViewChild('ionInputEl', { static: true }) ionInputEl!: IonInput;
 
@@ -66,4 +45,12 @@ export class EditProfileCompanyPage implements OnInit {
     this.ionInputEl.value = this.inputModel = filteredValue;
   }
 
+  //Validaciones
+  validateProfile(){
+    const emailValidations:any = emailValidation(this.user.email);
+    this.emailIsCorrect = emailValidations.allOk;
+    this.nameIsCorrect = textValidaton(this.user.name);
+    
+    this.emailErrorMessage = emailValidations.errorMessage;
+  }
 }
